fb3a59ef422a1eabff196ae8f3837d06c565a965
Dave Allie <dave@daveallie.com>
2026-02-11 00:24:12 +1100

    Fix typo

--- src/activities/network/CrossPointWebServerActivity.cpp
+++ src/activities/network/CrossPointWebServerActivity.cpp
@@ -348,7 +348,7 @@ void CrossPointWebServerActivity::loop() {
         // Yield and check for exit button every 64 iterations
         if ((i & 0x3F) == 0x3F) {
           yield();
-          // Force trigger an update of which buttons are being pressed so be have accurate stat
+          // Force trigger an update of which buttons are being pressed so be have accurate state
           // for back button checking
           mappedInput.update();
           // Check for exit button inside loop for responsiveness
